1823f9c04b920999659b1daf76116c0f9ca6d1d3
cephas <cephas.omaku@gmail.com>
2021-07-06 17:05:53 +0100

    Added Colours Black, Orange and Navy Blue (Wont Need the Color Palette Feature)

--- inc/gutenberg.php
+++ inc/gutenberg.php
@@ -2,7 +2,7 @@
 
 	//Custom Gutenberg Function
 	function twitpos_default_cover_color ()
-		{
+			{
 			add_theme_support( 
 				'editor-color-palette',
 					// code...
@@ -11,9 +11,24 @@
 						'name' => 'White',
 						'slug' => 'white',
 						'color' => '#ffffff'
+					),
+						array(
+						'name' => 'Black',
+						'slug' => 'black',
+						'color' => '#000000'
+					),
+						array(
+						'name' => 'Orange',
+						'slug' => 'orange',
+						'color' => '#ffa500'
+					),
+						array(
+						'name' => 'Navy Blue',
+						'slug' => 'navy-blue',
+						'color' => '#000080'
+						)
 					)
-				)
-			 );
-	}
+				);
+			}
 
 	add_action( 'init', 'twitpos_default_cover_color' );
